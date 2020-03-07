06417d5bb0dc0393210cc95a367b158db23769da
Latte22 <61899747+Latte22@users.noreply.github.com>
2020-03-07 18:42:24 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,354 @@
+<?php
+// Query params are: utm_creative={{ad.name}}&utm_campaign={{campaign.name}}&utm_source={{site_source_name}}&utm_placement={{placement}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&adset_name={{adset.name}} 
+
+require_once dirname(__FILE__) . '/kclick_client.php';
+$client = new KClickClient('http://92.118.149.58/api.php?', 'NqSV3FzBdS5CJLRH');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
+<!DOCTYPE html>
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>BLACK LATTE - SECRETUL DELICIOS ȘI REVIGORANT AL SUPLEȚEI STARURILOR.</title>
+    
+    <!--pageMeta-->
+
+    <!-- Loading Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+	
+    <link href="css/magnific-popup.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/animate-custom.css"> 
+	  <link href="css/prettyPhoto.css" rel="stylesheet" type="text/css">
+    <link href="css/hover-min.css" rel="stylesheet" type="text/css">
+       
+    <!-- Loading Font Styles -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+
+	  <!-- Loading Elements Styles -->   
+    <link href="css/navigation-styles.css" rel="stylesheet">    	
+    <link href="css/jquery.bxslider.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet">
+	
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="scripts/html5shiv.js"></script>
+      <script src="scripts/respond.min.js"></script>
+    <![endif]-->
+    
+    <!--headerIncludes-->
+    
+<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/40/3/intl/ru_ALL/common.js"></script><script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/40/3/intl/ru_ALL/util.js"></script></head>
+<body data-spy="scroll" data-target=".navMenuCollapse">
+    
+    <div id="wrap">
+        
+    <!-- start header -->
+        	                                  
+		<!-- end header --><!--start features-->
+
+		 
+		<!--end features--><!--start gallery-->
+		
+        
+
+		<!--end gallery--><!-- FOOTER START -->
+    
+    <!-- FOOTER END --><!-- start header -->
+        	                                  
+		<!-- end header --><!--start features-->
+
+		 
+		<!--end features--><!--start gallery-->
+		
+        
+
+		<!--end gallery--><!-- FOOTER START -->
+    
+    <!-- FOOTER END --><!-- start header -->
+        	                                  
+		<!-- end header --><!--start features-->
+
+		 
+		<!--end features--><!--start gallery-->
+		
+        
+
+		<!--end gallery--><!-- FOOTER START -->
+    
+    <!-- FOOTER END --><!-- start header -->
+        <section class="ewbcraft-topbar1">
+			<div class="container">
+		    	<div class="row">
+		        	<div class="col-sm-4 header-base-block">
+		            	<div class="headder-block">
+		                <div class="col-sm-2 col-xs-2 header-icon text-right">
+		            			<img src="images/header-png2_03.png" alt="your turst our business">
+		                    </div>
+		                    <div class="col-sm-10 header-icon text-left">
+		                		<p>Compoziție naturală</p>
+		                    </div>
+		                </div>
+		            </div>
+		            
+		         <div class="col-sm-4 header-base-block">
+		            	<div class="header-rotate">
+		                	<div class="back-skew">
+		            		<a href="tel:0743307909" style="color: rgb(66, 66, 66); font-size: 14.6667px; background-color: rgba(0, 0, 0, 0);" src="images/uploads/Screenshot_26.png"><img src="images/header-png1_03.png" alt="phone#">
+		                	for details call: 254-256-988-1</a>
+		                    </div>
+		                </div>
+		            </div>
+		            
+		            <div class="col-sm-4 header-base-block">
+		            	<div class="headder-block text-center">
+		                	<div class="col-sm-2 col-xs-2 header-icon text-right">
+		            			<img src="images/header-png_03.png" alt="customer">
+		                    </div>
+		                    <div class="col-sm-10 header-icon text-left">
+		                		<p>27/4 fully coustomer services</p>
+		                    </div>
+		                </div>
+		            </div>
+		            
+		        </div>
+		    </div>
+		</section>	                                  
+		<!-- end header --><div class="module parallax ewebcraft-banner7">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-6 col-sm-6 banner-text text-left">
+                      <h3></h3>
+                      <hr>
+                      <h2>BLACK LATTE</h2>
+                      <h2>BLACK LATTE - SECRETUL DELICIOS ȘI REVIGORANT AL SUPLEȚEI STARURILOR.</h2>
+                      <p>LATTE CU CĂRBUNE PENTRU IUBITORII DE DULCE CARE VISEAZĂ SĂ SLĂBEASCĂa</p>
+                      
+                  </div>
+                  <div class="col-lg-6 col-sm-6 text-center hidden-xs">
+                        <img src="images/uploads/group.png" alt="banner right images">
+                  </div>
+              </div>
+          </div>
+      </div><br><br><br><section id="subscribe-2">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12 col-md-12 col-sm-12 news-letter-form">
+                <div class="col-xs-12 text-center news-latter-heading">
+                  <h3>Get Our Latest Updates</h3>
+                </div> 
+                <form class="form-inline col-sm-12">
+                  <div class="form-group col-sm-9">
+                    <div class="label-text col-md-4 col-sm-5">
+                      <label for="email">24/7 Be Updated</label>
+                    </div>
+                    <div class="input-field col-md-8 col-sm-7">
+                      <input type="email" placeholder="Enter Your Email ID" class="form-control" id="email">
+                    </div>
+                  </div>
+                  <div class="col-sm-3">
+                    <button type="submit" name="subscribe" value="subscribe" class="btn btn-default submit-button">Rezultat garantat la orice vârstă</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div> 
+        </section><!--start features-->
+
+		<section class="ewebcraft-features3">
+    <div class="container ">
+    	<div class="row">
+        <div class="col-md-6 col-sm-12 col-xs-12 tour-reviews">
+                    <ul class="tour-slider">
+                        <li><img src="images/uploads/cup.png" alt="Amazing Tours Review" style="width: 540px; height: 365px; border-radius: 0px; border-style: none; border-width: 1px;" rel="outline: red dashed 3px; cursor: pointer;"></li>
+                        <li>
+                        
+                        </li>
+                         <li>
+                        
+                        </li>
+                    </ul>
+                </div>
+        	<div class="col-md-6 col-sm-12 col-xs-12 tour-reviews">
+            	<div class="reviews-main-text">
+                	<h2>Cel mai popular produs pentru slăbire al starurilor!</h2>
+                    <p>Nutriționistul Sintya Pascal, consultantul în dietologie al Charlize Theron și Sandra Bullok recomandă pentru o pierdere rapidă în greutate să substituiți gustările și/sau una din mese cu latte cu cărbune. Acesta stimulează metabolismul, îți dă putere și energie pentru un ritm de viață activ și beneficii pentru siluetă. Subsituie mesele principale cu el timp de o lună. În acest timp, poți obține rezultate impresionante.</p>
+                    <ul>
+                    	<li><i class="fa fa-check"></i>Absoarbe grăsimea, transformând-o în energie</li>
+                    	<li><i class="fa fa-check"></i>Elimină toxinele din organism</li>
+                    	<li><i class="fa fa-check"></i>Suprimă pofta de mâncare și-ți oferă energie</li>
+                    	<li><i class="fa fa-check"></i>Elimină excesul de lichid din organism</li>
+                    	<li><i class="fa fa-check"></i>Produce dopamina (hormonul fericirii)</li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section> 
+		<!--end features--><section class="ewc-content-11">
+        <div class="container">
+            <div class="row">
+                <div class="learn_main text-center">
+                    <h2>BLACK LATTE - SECRETUL DELICIOS ȘI REVIGORANT AL SUPLEȚEI STARURILOR.</h2>
+                      <hr>
+                  </div>
+                  
+                  <div class="col-sm-6 learn_block">
+                    <p>Oamenii de știință americani au gasit in sfârșit o modalitate simplă și 
+confortabilă de a slăbi fără a schimba modul de viață obișnuit 
+<strong>Au elaborat formula băuturii Black Latte </strong>, are același gust al băuturii preferate, dar conține <strong>cărbune activat care absoarbe activ grăsimea </strong>și o descompune, elimină toxinele din organism. Kilogramele se topesc, dar sânii nu se micșorează. <strong>Black Latte intensifică metabolismul și inhibă apetitul </strong>, fapt ce permite să pierzi în mod constant câte kgs pe săptămână fără exerciții fizice suplimentare sau restricții dietetice.</p>
+                      
+                      <div class="col-sm-6 learn-item text-center">
+                        <div class="icon c3 ">1</div>
+                          <h4></h4><h2 id="h.p_5Svn0i6KsdGP" class="zfr3Q JYVBee" style="direction: ltr; text-align: center;" tabindex="-1">CARBON ACTIVAT</h2>
+                      </div>
+                      
+                      <div class="col-sm-6 learn-item text-center">
+                        <div class="icon c5 ">2</div>
+                          <h4></h4><h2 id="h.p_YkFNCzNNsdGj" class="zfr3Q JYVBee" style="direction: ltr; text-align: center;" tabindex="-1">LAPTE DE NUCĂ DE COCOS</h2>
+                      </div>
+                      
+                      <div class="col-sm-6 learn-item text-center">
+                        <div class="icon ">3</div>
+                          <h4></h4><h2 id="h.p_qFnWnLBiukDO" class="zfr3Q JYVBee" style="direction: ltr; text-align: center;" tabindex="-1">L-CARNITINE </h2>
+                      </div>
+                      
+                      <div class="col-sm-6 learn-item  text-center">
+                        <div class="icon c1 ">4</div>
+                          <h4></h4><h2 id="h.p_feqvtJgKukDW" class="zfr3Q JYVBee" style="direction: ltr; text-align: center;" tabindex="-1">OMEGA 3</h2>
+                      </div>
+                  </div>
+                  
+                  <div class="col-sm-6 image">
+                    <img src="images/uploads/group.png" alt="how it work" style="width: 555px; height: 428px; border-radius: 0px; border-style: none; border-width: 1px;">
+                  </div>
+              </div>
+          </div>
+      </section><!--start gallery-->
+		
+        <section class="ewc-gallery1" style="outline-offset: -3px;">
+			<div class="container">
+		    	<div class="row">
+		        	<div class="gallary_main_text text-center">
+		            	<h3>EVENT gallery</h3>
+		                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+		            </div>
+		        </div>
+		    </div>
+		    <div class="container-fluid">
+		    	
+		        	<div class="gallary_base_block">
+		            	<img src="images/uploads/Screenshot_25.png" alt="EVENT gallery" style="width: 358px; height: 311px; border-radius: 0px; border-style: none; border-width: 1px;">
+		                <div class="gallary_hover_icon">
+		               <a href="./images/Eventer_06.jpg" rel="prettyPhoto[images-gallery]"><i class="fa fa-search"></i></a>
+		                </div>
+		            </div>
+		            <div class="gallary_base_block">
+		            	<img src="images/uploads/Screenshot_24.png" alt="EVENT gallery" style="width: 358px; height: 309px; border-radius: 0px; border-style: none; border-width: 1px;">
+		                <div class="gallary_hover_icon">
+		                <a href="./images/Eventer_07.jpg" rel="prettyPhoto[images-gallery]"><i class="fa fa-search"></i></a>
+		                </div>
+		            </div>
+		            <div class="gallary_base_block">
+		            	<img src="images/uploads/Screenshot_27.png" alt="EVENT gallery" style="width: 358px; height: 310px; border-radius: 0px; border-style: none; border-width: 1px;">
+		                <div class="gallary_hover_icon">
+		               <a href="./images/Eventer_08.jpg" rel="prettyPhoto[images-gallery]"><i class="fa fa-search"></i></a>
+		                </div>
+		            </div>
+		            <div class="gallary_base_block">
+		            	<img src="images/uploads/Screenshot_28.png" alt="EVENT gallery" style="width: 358px; height: 310px; border-radius: 0px; border-style: none; border-width: 1px;">
+		                 <div class="gallary_hover_icon">
+		               <a href="./images/Eventer_09.jpg" rel="prettyPhoto[images-gallery]"><i class="fa fa-search"></i></a>
+		                </div>
+		            </div>
+		            <div class="gallary_base_block">
+		            	<img src="images/uploads/Screenshot_26.png" alt="EVENT gallery" style="width: 358px; height: 311px; border-radius: 0px; border-style: none; border-width: 1px;">
+		                <div class="gallary_hover_icon">
+		                 <a href="./images/Eventer_10.jpg" rel="prettyPhoto[images-gallery]"><i class="fa fa-search"></i></a>
+		                </div>
+		            </div>
+		       
+		    </div>
+		</section>
+
+		<!--end gallery--><section id="news-letter" class="ewc-news-letter-1" style="outline-offset: -3px;">
+          <div class="container">
+            <div class="row text-center">
+              <h1 class="section-heading wow fadeInUp" data-wow-duration="2s">
+                Newsletter
+              </h1>
+              <p class="wow fadeInUp" data-wow-duration="2s">
+                Nunc lorem mi, pulvinar quis Class aptent taciti molestie tincidunt.
+              </p>
+            </div>
+            <div class="row news-letter-form">
+              <div class="col-sm-9 col-sm-offset-0 col-lg-7 col-lg-offset-2 col-xs-12 col-xs-offset-0 wow fadeInUp" data-wow-duration="2s">
+                    <form class="form-inline col-xs-12">
+                      <div class="form-group">              
+                        <div class="col-sm-11 col-xs-7 form-input">
+                          <label class="sr-only" for="userEmail">Email Address</label>
+                          <input type="email" class="form-control" id="userEmail" placeholder="Email Adrress" required="">
+                        </div>  
+                        <div class="col-sm-1 col-xs-4 form-input">
+                          <button type="submit" class="btn btn-default submit-button">
+                            Join us Now!
+                          </button>
+                        </div>                
+                      </div>
+                    </form>
+                  </div>
+            </div>
+          </div>
+        </section><!-- FOOTER START -->
+    <section class="ewc-footer-4">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-3 col-sm-6 footer-text">
+                  <p>©2019 Spa Green All Rights Reserved</p>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-lg-offset-6 footer-text">
+                  <a href="https://themeforest.net/user/ewebcraft">Designed &amp; Developed By: BLACK LATTE</a>
+                </div>
+            </div>
+        </div> 
+    </section>
+    <!-- FOOTER END --></div><!-- /#wrap -->	 
+
+    <!-- JavaScript --> 
+	<script src="scripts/jquery.js" type="text/javascript"></script>
+	<script src="scripts/bootstrap.min.js" type="text/javascript"></script>
+	<script src="scripts/smoothScroll.js" type="text/javascript"></script>
+	<script src="scripts/waypoints.js" type="text/javascript"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+  <script src="scripts/jquery.prettyPhoto.js" type="text/javascript"></script>
+  <script src="scripts/jquery.bxslider.js" type="text/javascript"></script>
+  <script src="scripts/jquery.counterup.js" type="text/javascript"></script>
+	<script src="scripts/script.js" type="text/javascript"></script>
+
+  <script type="text/javascript">
+    jQuery(function() {
+        $('form.contact-form').on('submit', function(){
+          var curForm = jQuery(this);
+          $.ajax({
+            url: 'mail/mail.php',
+            type: 'POST',
+            data: jQuery(curForm).serialize(),
+            success: function(data) {
+              jQuery(curForm).children('.expMessage').html(data);
+            }
+          });
+          return false;
+        }); 
+    });
+  </script>	
+	
+
+
+</body></html>
